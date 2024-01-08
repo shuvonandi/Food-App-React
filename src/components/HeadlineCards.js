80459f6ac0e7e09bd10ae42e2ea748f72cb39ed9
shuvonandi <shuvonandi82@gmail.com>
2024-01-09 01:51:49 +0600

    Food-App-React

---
+++ src/components/HeadlineCards.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import images from '../Assets/sa.jpg'
+import sb from '../Assets/sb.jpg'
+import sc from '../Assets/sc.jpeg'
+
+const HeadlineCards = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        {/* {Card} */}
+        <div className='rounded-xl relative'>
+            {/* {Overlay} */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+                <p className='px-2'>Through 8/26</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Ordar Now</button>
+            </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={images} alt='/' />
+        </div>
+        {/* {Card} */}
+        <div className='rounded-xl relative'>
+            {/* {Overlay} */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
+                <p className='px-2'>Added Daily</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Ordar Now</button>
+            </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={sb} alt='/' />
+        </div>
+        {/* {Card} */}
+        <div className='rounded-xl relative'>
+            {/* {Overlay} */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
+                <p className='px-2'>Tasty Treats</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Ordar Now</button>
+            </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={sc} alt='/' />
+        </div>
+    </div>
+  )
+}
+
+export default HeadlineCards
